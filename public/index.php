ae42b8f1aeefe821a5e1b28f9df2b1fb074cbfbd
muhbatni <ibat366@gmail.com>
2026-02-03 12:23:19 +0700

    update location

--- public/index.php
+++ public/index.php
@@ -4,11 +4,11 @@ session_start();
 // Cek apakah user sudah login
 if (isset($_SESSION['user_id'])) {
     // Sudah login → ke homepage
-    header("Location: homepage.php");
+    header("Location: ../pages/homepage.php");
     exit;
 } else {
     // Belum login → ke login
-    header("Location: login.php");
+    header("Location: ../auth/login.php");
     exit;
 }
 ?>
